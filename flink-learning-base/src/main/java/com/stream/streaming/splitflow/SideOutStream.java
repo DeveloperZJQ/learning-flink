35e7dc2ffa00e8a620ebd4a2fa138dcbcd02b7f5
happy <18380463469@163.com>
2020-09-26 23:42:08 +0800

    no commit

--- flink-learning-base/src/main/java/com/stream/streaming/splitflow/SideOutStream.java
+++ flink-learning-base/src/main/java/com/stream/streaming/splitflow/SideOutStream.java
@@ -1,5 +1,6 @@
 package com.stream.streaming.splitflow;
 
+import com.alibaba.fastjson.JSON;
 import com.happy.common.model.MetricEvent;
 import com.happy.common.utils.KafkaConfigUtil;
 import org.apache.flink.streaming.api.datastream.DataStream;
@@ -15,11 +16,11 @@ import org.apache.flink.util.OutputTag;
  * @create 2020-09-03 23:35
  */
 public class SideOutStream {
-    private static final OutputTag<String> aaaTag = new OutputTag<>("aaa");
-    private static final OutputTag<String> bbbTag = new OutputTag<>("bbb");
-    private static final OutputTag<String> cccTag = new OutputTag<>("ccc");
+    private static final OutputTag<String> aaaTag = new OutputTag<>("aaa"){};
+    private static final OutputTag<String> bbbTag = new OutputTag<>("bbb"){};
+    private static final OutputTag<String> cccTag = new OutputTag<>("ccc"){};
 
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         DataStreamSource<MetricEvent> metricEventDataStreamSource = KafkaConfigUtil.buildSource(env);
@@ -30,20 +31,20 @@ public class SideOutStream {
                 String s = metricEvent.getTags().get("type");
                 switch (s) {
                     case "machine":
-                        context.output(machineTag, metricEvent);
+                        context.output(aaaTag, JSON.toJSONString(metricEvent));
                     case "docker":
-                        context.output(dockerTag, metricEvent);
+                        context.output(bbbTag,JSON.toJSONString(metricEvent));
                     case "application":
-                        context.output(applicationTag, metricEvent);
+                        context.output(cccTag,JSON.toJSONString(metricEvent));
                     default:
                         collector.collect(metricEvent);
                 }
             }
         });
 
-        DataStream<MetricEvent> docker      = sideOutputResult.getSideOutput(dockerTag);
-        DataStream<MetricEvent> application = sideOutputResult.getSideOutput(applicationTag);
-        DataStream<MetricEvent> machine     = sideOutputResult.getSideOutput(machineTag);
+        DataStream<String> docker      = sideOutputResult.getSideOutput(aaaTag);
+        DataStream<String> application = sideOutputResult.getSideOutput(bbbTag);
+        DataStream<String> machine     = sideOutputResult.getSideOutput(cccTag);
 
         docker.print();
         application.print();
