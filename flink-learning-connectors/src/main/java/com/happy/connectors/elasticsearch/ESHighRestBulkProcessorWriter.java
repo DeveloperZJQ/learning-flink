9977d7e47450ed6729942fe0cb700af5d081410d
DeveloperZJQ <18380463469@163.com>
2020-11-12 16:33:29 +0800

    新增elasticsearch BulkProcessor

---
+++ flink-learning-connectors/src/main/java/com/happy/connectors/elasticsearch/ESHighRestBulkProcessorWriter.java
@@ -0,0 +1,119 @@
+package com.happy.connectors.elasticsearch;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.action.bulk.BulkProcessor;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.threadpool.ThreadPool;
+
+import java.util.Properties;
+
+/**
+ * @author happy
+ * @since 2020-11-12
+ */
+@Slf4j
+public class ESHighRestBulkProcessorWriter extends RichSinkFunction<String> {
+    private Properties para;
+    private static RestHighLevelClient restHighLevelClient = null;
+    private static BulkProcessor bulkProcessor = null;
+
+    @Override
+    public void open(Configuration parameters) throws Exception {
+        initESClient();
+        initBulkProcessor();
+    }
+
+    @Override
+    public void invoke(String value, Context context) throws Exception {
+        String indexName = "test001";
+        String type = "banji";
+        String id = "123213213";
+        bulkProcessor.add(new IndexRequest(indexName,type,id).source(value));
+    }
+
+    @Override
+    public void close() throws Exception {
+        super.close();
+        if (bulkProcessor!=null){
+            bulkProcessor.close();
+        }
+    }
+
+    void initESClient() {
+        BasicCredentialsProvider basicCredentialsProvider = new BasicCredentialsProvider();
+        basicCredentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("es.cluster.name", "pwd"));
+
+        String[] esHost = "192.168.1.11,192.168.1.12,192.168.1.13".split(",");
+        HttpHost[] httpHosts = new HttpHost[esHost.length];
+
+        for (int i = 0; i < esHost.length; i++) {
+            httpHosts[i] = new HttpHost(esHost[i], 9200, "http");
+        }
+
+        restHighLevelClient = new RestHighLevelClient(
+                RestClient.builder(httpHosts)
+                .setRequestConfigCallback(new RestClientBuilder.RequestConfigCallback() {
+                    @Override
+                    public RequestConfig.Builder customizeRequestConfig(RequestConfig.Builder builder) {
+                        builder.setConnectionRequestTimeout(10000);
+                        builder.setConnectTimeout(10000);
+                        builder.setSocketTimeout(10000);
+                        return builder;
+                    }
+                })
+                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpAsyncClientBuilder) {
+                        httpAsyncClientBuilder.disableAuthCaching();
+                        httpAsyncClientBuilder.setMaxConnPerRoute(3);
+                        httpAsyncClientBuilder.setMaxConnTotal(100);
+                        return httpAsyncClientBuilder.setDefaultCredentialsProvider(basicCredentialsProvider);
+                    }
+                }).build()
+        );
+    }
+
+
+    /**
+     * 初始化BulkProcessor
+     */
+    void initBulkProcessor() {
+        Settings settings = Settings.EMPTY;
+        ThreadPool threadPool = new ThreadPool(settings);
+        BulkProcessor.Listener listener = new BulkProcessor.Listener() {
+            @Override
+            public void beforeBulk(long l, BulkRequest bulkRequest) {
+                int numberOfActions = bulkRequest.numberOfActions();
+                log.info("Executing bulk [{}] with {} requests", l, numberOfActions);
+            }
+
+            @Override
+            public void afterBulk(long l, BulkRequest bulkRequest, BulkResponse bulkResponse) {
+                if (bulkResponse.hasFailures()) {
+                    log.warn("Bulk [{}] executed with failure", l);
+                } else {
+                    log.info("Bulk [{}] completed in {} milliseconds", l, bulkResponse.getTook().getMillis());
+                }
+            }
+
+            @Override
+            public void afterBulk(long l, BulkRequest bulkRequest, Throwable throwable) {
+                log.error("Failed to executed bulk", throwable);
+            }
+        };
+    }
+}
---
+++ flink-learning-connectors/src/main/java/com/happy/connectors/elasticsearch/ESMain.java
@@ -0,0 +1,43 @@
+package com.happy.connectors.elasticsearch;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+import org.apache.flink.util.Collector;
+
+import java.util.Properties;
+
+/**
+ * @author happy
+ * @since 2020-11-12
+ */
+@Slf4j
+public class ESMain {
+    public static void main(String[] args) throws Exception {
+
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        //Properties参数定义
+        Properties props = new Properties();
+        props.put("bootstrap.servers", args[0]);
+        props.put("group.id", "metric-group");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");//key反序列化
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");//value反序列化
+
+        props.put("auto.offset.reset", "latest");//偏移量最新earliest
+
+        DataStreamSource<String> metricStreamSource = env.addSource(new FlinkKafkaConsumer011<>(
+                "test",   //kafka topic
+                new SimpleStringSchema(),//String 序列化
+                props
+        )).setParallelism(1);
+
+        //批量写入elasticsearch
+        metricStreamSource.addSink(new ESHighRestBulkProcessorWriter()).name("SinkES");
+        env.execute("Flink DataSource");
+    }
+}
