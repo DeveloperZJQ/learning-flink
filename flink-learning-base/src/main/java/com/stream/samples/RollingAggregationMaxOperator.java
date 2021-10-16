37b70eea11b814abb29d96d8ed77bf86958ddd91
happy <18380463469@163.com>
2021-10-16 16:37:56 +0800

    update 滚动算子 RollingAggregationMaxOperator

--- flink-learning-base/src/main/java/com/stream/samples/RollingAggregationMaxOperator.java
+++ flink-learning-base/src/main/java/com/stream/samples/RollingAggregationMaxOperator.java
@@ -3,13 +3,8 @@ package com.stream.samples;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.functions.ReduceFunction;
-import org.apache.flink.api.common.typeinfo.TypeInformation;
-import org.apache.flink.api.common.typeinfo.Types;
-import org.apache.flink.api.java.functions.KeySelector;
-import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
-import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
@@ -23,48 +18,34 @@ public class RollingAggregationMaxOperator {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         DataStreamSource<String> dataStream = env.socketTextStream("127.0.0.1", 25432);
 
-//        SingleOutputStreamOperator<Tuple2<String, String>> maxRes = dataStream
-//                .filter((FilterFunction<String>) String::isEmpty)
-//                .map((MapFunction<String, Tuple2<String, String>>) s -> {
-//                    String[] split = s.split(",");
-//                    return Tuple2.of(split[0], split[1]);
-//                })
-//                .keyBy(0)
-//                .maxBy(1);
+        SingleOutputStreamOperator<Tuple2<String, String>> maxRes = dataStream
+                .filter((FilterFunction<String>) String::isEmpty)
+                .map((MapFunction<String, Tuple2<String, String>>) s -> {
+                    String[] split = s.split(",");
+                    return Tuple2.of(split[0], split[1]);
+                })
+                .keyBy(0)
+                .maxBy(1);
 
         SingleOutputStreamOperator<Tuple2<String, Integer>> reduce = dataStream
                 .filter((FilterFunction<String>) s -> !s.isEmpty())
-                .map(new MapFunction<String, Tuple2<String,Integer>>() {
-                    @Override
-                    public Tuple2<String, Integer> map(String s) throws Exception {
-
-                        return Tuple2.of(s,1);
-                    }
-                })
+                .map((MapFunction<String, Tuple2<String, Integer>>) s -> Tuple2.of(s, 1))
                 .keyBy(0)
-                .reduce(new ReduceFunction<Tuple2<String, Integer>>() {
-                    @Override
-                    public Tuple2<String, Integer> reduce(Tuple2<String, Integer> t1, Tuple2<String, Integer> t2) throws Exception {
-                        return Tuple2.of(t1.f0, t1.f1 + t2.f1);
-                    }
-                });
+                .reduce((ReduceFunction<Tuple2<String, Integer>>) (t1, t2) -> Tuple2.of(t1.f0, t1.f1 + t2.f1));
 
         dataStream
                 .filter((FilterFunction<String>) s -> !s.isEmpty())
-                .map(new MapFunction<String, Tuple2<String,Integer>>() {
-                    @Override
-                    public Tuple2<String, Integer> map(String s) throws Exception {
-                        String[] word = s.split(",");
-                        String s1 = word[0];
-                        Integer s2 = Integer.parseInt(word[1]);
-                        return Tuple2.of(s1,s2);
-                    }
+                .map((MapFunction<String, Tuple2<String, Integer>>) s -> {
+                    String[] word = s.split(",");
+                    String s1 = word[0];
+                    Integer s2 = Integer.parseInt(word[1]);
+                    return Tuple2.of(s1, s2);
                 })
                 .keyBy(0)
                 .max(1).print();
 
+        maxRes.print();
         reduce.print();
-
         env.execute(RollingAggregationMaxOperator.class.getSimpleName());
     }
 }
--- flink-learning-base/src/main/java/com/stream/watermark/PeriodWatermark.java
+++ flink-learning-base/src/main/java/com/stream/watermark/PeriodWatermark.java
@@ -8,32 +8,34 @@ import org.apache.flink.streaming.api.watermark.Watermark;
 
 import javax.annotation.Nullable;
 
+import java.util.Objects;
+
 import static com.happy.common.utils.DateUtil.YYYY_MM_DD_HH_MM_SS;
 
 /**
  * @author happy
- * @create 2020-07-11 16:30
+ * @since 2020-07-11 16:30
  */
 @Slf4j
 public class PeriodWatermark implements AssignerWithPeriodicWatermarks<TransInfo> {
-    private Long currentTimestamp   =   Long.MIN_VALUE;
+    private Long currentTimestamp = Long.MIN_VALUE;
 
     @Nullable
     @Override
     public Watermark getCurrentWatermark() {
-        long maxTimeLag =   5000;       //最大延时5s
-        return new Watermark(currentTimestamp==Long.MIN_VALUE?Long.MIN_VALUE:currentTimestamp-maxTimeLag);
+        long maxTimeLag = 5000;       //最大延时5s
+        return new Watermark(currentTimestamp == Long.MIN_VALUE ? Long.MIN_VALUE : currentTimestamp - maxTimeLag);
     }
 
     @Override
     public long extractTimestamp(TransInfo transInfo, long l) {
-        log.info("extractTimestamp transInfo:"+transInfo.toString()+" ,l:"+l);
+        log.info("extractTimestamp transInfo:" + transInfo.toString() + " ,l:" + l);
 
-        long timestamp      =   transInfo.getTimeStamp();
-        currentTimestamp    =   Math.max(timestamp,currentTimestamp);
+        long timestamp = transInfo.getTimeStamp();
+        currentTimestamp = Math.max(timestamp, currentTimestamp);
         log.info("event timestamp = {}, {}, CurrentWatermark = {}, {}", transInfo.getTimeStamp(),
                 DateUtil.format(transInfo.getTimeStamp(), YYYY_MM_DD_HH_MM_SS),
-                getCurrentWatermark().getTimestamp(),
+                Objects.requireNonNull(getCurrentWatermark()).getTimestamp(),
                 DateUtil.format(getCurrentWatermark().getTimestamp(), YYYY_MM_DD_HH_MM_SS));
         return transInfo.getTimeStamp();
     }
