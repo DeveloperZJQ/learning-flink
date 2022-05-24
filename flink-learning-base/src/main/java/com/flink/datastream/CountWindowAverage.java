3d9e20fbc79e777d9c3a4647b6c03d8bf51df38e
ZhangJianqiang <zjq@idatafun.com>
2022-05-24 22:44:20 +0800

    增加状态ttl

--- flink-learning-base/src/main/java/com/flink/datastream/CountWindowAverage.java
+++ flink-learning-base/src/main/java/com/flink/datastream/CountWindowAverage.java
@@ -1,9 +1,15 @@
 package com.flink.datastream;
 
 import org.apache.flink.api.common.functions.RichFlatMapFunction;
+import org.apache.flink.api.common.state.StateTtlConfig;
 import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.time.Time;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
 
 /**
@@ -11,25 +17,65 @@ import org.apache.flink.util.Collector;
  * @since 2022/5/24
  */
 public class CountWindowAverage extends RichFlatMapFunction<Tuple2<Long, Long>, Tuple2<Long, Long>> {
+
     /**
      * The ValueState handle. The first field is the count, the second field a running sum.
      */
     private transient ValueState<Tuple2<Long, Long>> sum;
 
     @Override
-    public void flatMap(Tuple2<Long, Long> longLongTuple2, Collector<Tuple2<Long, Long>> collector) throws Exception {
+    public void flatMap(Tuple2<Long, Long> input, Collector<Tuple2<Long, Long>> out) throws Exception {
+
         // access the state value
-        Tuple2<Long, Long> value = sum.value();
+        Tuple2<Long, Long> currentSum = sum.value();
 
-        //update the count
-        value.f0 += 1;
+        // update the count
+        currentSum.f0 += 1;
 
-        //add the second field of the input value
+        // add the second field of the input value
+        currentSum.f1 += input.f1;
 
+        // update the state
+        sum.update(currentSum);
+
+        // if the count reaches 2, emit the average and clear the state
+        if (currentSum.f0 >= 2) {
+            out.collect(new Tuple2<>(input.f0, currentSum.f1 / currentSum.f0));
+            sum.clear();
+        }
     }
 
     @Override
-    public void open(Configuration parameters) throws Exception {
-        super.open(parameters);
+    public void open(Configuration config) {
+        StateTtlConfig ttlConfig = StateTtlConfig
+                .newBuilder(Time.seconds(1))
+                .setUpdateType(StateTtlConfig.UpdateType.OnCreateAndWrite)
+                .setStateVisibility(StateTtlConfig.StateVisibility.NeverReturnExpired)
+                .build();
+
+        ValueStateDescriptor<Tuple2<Long, Long>> descriptor =
+                new ValueStateDescriptor<>(
+                        "average", // the state name
+                        TypeInformation.of(new TypeHint<>() {
+                        }), // type information
+                        Tuple2.of(0L, 0L)); // default value of the state, if nothing was set
+
+        //设置每条独立keyed dataStream状态的生存时间
+        descriptor.enableTimeToLive(ttlConfig);
+        sum = getRuntimeContext().getState(descriptor);
+    }
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // this can be used in a streaming program like this (assuming we have a StreamExecutionEnvironment env)
+        env.fromElements(Tuple2.of(1L, 3L), Tuple2.of(1L, 5L), Tuple2.of(1L, 7L), Tuple2.of(1L, 4L), Tuple2.of(1L, 2L))
+                .keyBy(value -> value.f0)
+                .flatMap(new CountWindowAverage())
+                .print();
+
+        // the printed output will be (1,4) and (1,5)
+        env.execute();
     }
 }
+
